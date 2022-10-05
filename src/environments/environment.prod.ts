9632560f84c0d9b7315798bca2247d9adfb2f461
syahridomualim <syahridomualim@gmail.com>
2022-10-05 22:44:02 +0700

    build project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://usermanagement-env.eba-nmqucfgk.us-east-2.elasticbeanstalk.com/'
+  apiUrl: 'http://usermanagement-env.eba-nmqucfgk.us-east-2.elasticbeanstalk.com'
 };
